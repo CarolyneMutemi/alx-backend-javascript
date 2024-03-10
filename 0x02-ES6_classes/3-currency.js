830f03f5bd6f00ff20594bbf7ea3b4365235c047
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-11 00:46:32 +0300

    Added Setters and Getters

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,30 +1,42 @@
 /* eslint-disable no-throw-literal */
 
 export default class Currency {
-    constructor(code, name){
-        Currency.verifyCode(code)
-        Currency.verifyName(name)
-        this._code = code
-        this._name = name
-    }
+  constructor(code, name) {
+    Currency.verifyCode(code);
+    Currency.verifyName(name);
+    this._code = code;
+    this._name = name;
+  }
 
-    static verifyCode(code)
-    {
-        if (typeof(code) !== "string")
-        {
-            throw ("TypeError: Code must be a string")
-        }
-    }
+  get code() {
+    return this._code;
+  }
 
-    static verifyName(name)
-    {
-        if (typeof(name) !== "string")
-        {
-            throw ("TypeError: Name must be a string")
-        }
-    }
+  get name() {
+    return this._name;
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`
+  set name(name) {
+    this._name = name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  static verifyCode(code) {
+    if (typeof (code) !== 'string') {
+      throw ('TypeError: Code must be a string');
     }
+  }
+
+  static verifyName(name) {
+    if (typeof (name) !== 'string') {
+      throw ('TypeError: Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
