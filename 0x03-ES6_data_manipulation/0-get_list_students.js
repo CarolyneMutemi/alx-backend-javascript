c090c8cf4742ecf22213aebe693949a81bd4f3d9
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-12 18:55:39 +0300

    Linting

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
-export default function getListStudents() {
-    const firstStudent = createStudent(1, 'Guillaume', 'San Francisco')
-    const secondStudent = createStudent(2, 'James', 'Columbia')
-    const thirdStudent = createStudent(5, 'Serena', 'San Francisco')
-
-    return [ firstStudent, secondStudent, thirdStudent ]
+function createStudent(id, firstName, location) {
+  return { id, firstName, location };
 }
 
-function createStudent(id, firstName, location) {
-    return {id: id, firstName: firstName, location: location}
+export default function getListStudents() {
+  const firstStudent = createStudent(1, 'Guillaume', 'San Francisco');
+  const secondStudent = createStudent(2, 'James', 'Columbia');
+  const thirdStudent = createStudent(5, 'Serena', 'San Francisco');
+
+  return [firstStudent, secondStudent, thirdStudent];
 }
