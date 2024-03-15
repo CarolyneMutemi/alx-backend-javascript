1dd09cbfc47619908c8a835e9e1f5ed9c9d71441
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-15 23:37:54 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+    const wordList = []
+
+    for (const word of set){
+        if (word.startsWith(startString) && word[0] === startString[0]){
+            wordList.push(word.slice(startString.length))
+        }
+    }
+
+    return wordList.join('-')
+}
