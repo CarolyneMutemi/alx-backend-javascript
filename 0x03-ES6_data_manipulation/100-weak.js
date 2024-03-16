c1148cfa02c7aa746c76fea2a544d38b20e31dcc
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-16 06:05:46 +0300

    Weak link

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-throw-literal */
+
+export const weakMap = new WeakMap();
+
+let counter = 0;
+
+export function queryAPI(endpoint) {
+  if (counter >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  counter += 1;
+  weakMap.set(endpoint, counter);
+}
