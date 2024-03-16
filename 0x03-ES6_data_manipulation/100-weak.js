753526bd7923c37fc105fab8e7b8b1b0ea9f10bd
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-16 14:01:30 +0300

    Error check

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,9 +5,9 @@ export const weakMap = new WeakMap();
 let counter = 0;
 
 export function queryAPI(endpoint) {
+  counter += 1;
   if (counter >= 5) {
     throw new Error('Endpoint load is high');
   }
-  counter += 1;
   weakMap.set(endpoint, counter);
 }
