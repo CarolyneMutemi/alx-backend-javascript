9706a6ef6dab0501f822f2be474a3116b6581ab6
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-16 14:20:18 +0300

    Checking element type.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-continue */
+
 export default function cleanSet(set, startString) {
   const wordList = [];
 
   for (const word of set) {
+    if (typeof (word) !== 'string') {
+      continue;
+    }
     if (word.startsWith(startString) && word[0] === startString[0]) {
       wordList.push(word.slice(startString.length));
     }
