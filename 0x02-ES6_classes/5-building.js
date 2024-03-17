f189ed63620f3d971c2435edfac22bec3a02317c
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-03-17 23:13:19 +0300

    Building class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-throw-literal */
+/* eslint-disable class-methods-use-this */
+
+export default class Building {
+  constructor(sqft) {
+    Building.verifyIsNumber(sqft, 'Square feet');
+    this._sqft = sqft;
+    this.constructor.ensureMethodIsOverridden();
+  }
+
+  static verifyIsNumber(attribute, attributeName) {
+    if (typeof (attribute) !== 'number') {
+      throw (`TypeError: ${attributeName} must be a number`);
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    Building.verifyIsNumber(sqft, 'Square feet');
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {}
+
+  static ensureMethodIsOverridden() {
+    if (!Object.hasOwn(this.prototype, 'evacuationWarningMessage')) {
+      throw ('Error: Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
