2130e6b48033c07b09f19c63f9d1fa7ea5539cf5
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-20 02:52:11 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,9 @@
-import { uploadPhoto, createUser } from "./utils";
-export default function handleProfileSignup(){
-    Promise.all([uploadPhoto(), createUser()]).then((values) => {
-        console.log(values[0].body, values[1].firstName, values[1].lastName)
-    }).catch((error) => {
-        console.log(error)
-    })
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  }).catch((error) => {
+    console.log(error);
+  });
 }
