193625bc1fcac051614411311fea2069fcbfd55e
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-20 03:09:58 +0300

    Account for every resolution

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
-  promise.then((response) => ({ status: 200, body: 'success' })).then((response) => {
+  promise.then((response) => ({ status: 200, body: 'success' }))
+  .catch((error) => new Error())
+  .then((response) => {
     console.log('Got a response from the API');
-  }).catch((error) => new Error());
+  });
 }
