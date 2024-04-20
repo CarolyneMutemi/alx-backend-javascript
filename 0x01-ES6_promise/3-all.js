186480186d48cb29279abbcece8d22583d698bdc
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-20 03:17:52 +0300

    Catch eror

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(values[0].body, values[1].firstName, values[1].lastName);
   }).catch((error) => {
-    console.log(error);
+    console.log('Signup system offline');
   });
 }
