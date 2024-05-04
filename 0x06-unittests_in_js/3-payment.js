11f3340971e7a91f039bc078c8e3c3f0bf722700
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-05-04 19:59:20 +0300

    Right message.

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,11 +1,10 @@
 const { util } = require("chai");
 const Utils = require("./utils");
 
-//const utils = new Utils()
 
 function sendPaymentRequestToApi(totalAmount, totalShipping){
     const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping)
-    console.log(`The total is ${sum}`)
+    console.log(`The total is: ${sum}`)
 }
 
 module.exports = sendPaymentRequestToApi
